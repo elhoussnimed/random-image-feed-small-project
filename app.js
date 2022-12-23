4415d3409e9ba42edff02b9b64da950a5fcd4eae
mohammed el houssni <91427616+elhoussnimed@users.noreply.github.com>
2022-12-23 15:01:29 +0100

    add new api to get random image

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const imagesContainer = document.querySelector(".images");
-const imagesNumber = 24;
-imagesContainer.innerHTML = "";
+const imagesNumber = 30;
 
-function getRandomImage() {
-  const apiUrl = `https://api.unsplash.com//photos/random?client_id=iSjLUJ-JE_qHCNIaHiiswsoAZQJIECD2eyzy4Ikyrvw&count=24`;
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((images) => {
-      console.log(images);
-      images.forEach((image) => {
-        const imageUrl = image.urls.regular;
-        createImage(imageUrl);
-      });
-    })
-    .catch((error) => console.log("error", error));
+async function getRandomImage() {
+    const randomPage = Math.floor(Math.random() * (8000 / 50));
+    const apiUrl = `https://api.pexels.com/v1/curated?page=${randomPage}&per_page=${imagesNumber}`;
+    const authorizationKey = "563492ad6f917000010000012a5d103c1ece42d7b6f95b36df56a1fb";
+    const requestOption = {
+      method: "GET",
+      headers: {
+        Authorization: authorizationKey
+      }
+    }
+
+    const response = await fetch(apiUrl,requestOption);
+    const data = await response.json();
+    const {photos} = data;
+
+    photos.forEach(photo => {
+      const imageUrl = photo.src.landscape;
+      createImage(imageUrl)
+  })
 }
 
 function createImage(imageUrl) {
